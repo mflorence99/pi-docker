30576207a6fab8182dfac37750a34fb217ef1900
Mark Florence <mflo999@gmail.com>
2017-04-08 17:05:57 -0400

    Added .AppleDouble cleaner

--- scripts/pi-cleanup.sh
+++ scripts/pi-cleanup.sh
@@ -1,6 +1,7 @@
 docker volume ls -qf dangling=true | grep -v pi-* | xargs docker volume rm
 docker container rm -v $(docker ps -a -q -f status=exited)
 docker image rm $(docker images -f "dangling=true" -q)
+sudo find ./ -depth -name ".AppleDouble" -exec rm -Rf {} \;
 echo --- ; docker volume ls
 echo --- ; docker container ls -a
 echo --- ; docker image ls
