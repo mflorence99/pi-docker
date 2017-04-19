03f2fcf148ee56bee9ddd94f6a2a64d02dfc9d78
Mark Florence <mflo999@gmail.com>
2017-04-19 17:00:59 -0400

    Change to secure web sockets

--- scripts/pi-settings.sh
+++ scripts/pi-settings.sh
@@ -3,4 +3,4 @@ export DEV_DIR=$HOME/mflorence99
 export DOCKER_USERNAME=mflo999
 
 export PI_LED_HOST=https://raspberrypi:3000
-export PI_LED_WS=ws://raspberrypi:4000
+export PI_LED_WS=wss://raspberrypi:4000
