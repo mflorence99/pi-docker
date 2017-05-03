b357b8347c857e92e7161267b91336e946b2b434
Mark Florence <mflo999@gmail.com>
2017-05-03 17:04:55 -0400

    Vulcanize elements.html before running

--- scripts/pi-build.sh
+++ scripts/pi-build.sh
@@ -1,2 +1,3 @@
 ts-node tools/homify.ts --client
+gulp --gulpfile tools/vulcanize.ts --source "$(pwd)/client"
 ng build --prod --aot
--- scripts/pi-client.sh
+++ scripts/pi-client.sh
@@ -1,2 +1,3 @@
 ts-node tools/homify.ts --client
+gulp --gulpfile tools/vulcanize.ts --source "$(pwd)/client"
 ng serve --host 0.0.0.0
