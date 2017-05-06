0d152e49e71fc7568ac3d0b78573798b4626823f
Mark Florence <mflo999@gmail.com>
2017-05-06 04:22:12 -0400

    Added comments

--- scripts/pi-dockerize.sh
+++ scripts/pi-dockerize.sh
@@ -2,12 +2,14 @@
 
 find ./ -depth -name ".AppleDouble" -exec rm -Rf {} \;
 
-echo "Compressing dist ..."
-rm -f ./dist.tar.gz
-tar -zcf /tmp/dist.tar.gz ./
-mv /tmp/dist.tar.gz ./dist.tar.gz
+echo THIS WONT WORK UNTIL YOU MAKE IMAGE MANDATORY PARAMETER
 
-echo "Starting Docker build ..."
-docker build -t mflo999/pi-lib .
+# echo "Compressing dist ..."
+# rm -f ./dist.tar.gz
+# tar -zcf /tmp/dist.tar.gz ./
+# mv /tmp/dist.tar.gz ./dist.tar.gz
+#
+# echo "Starting Docker build ..."
+# docker build -t mflo999/pi-lib .
 
 # docker push mflo999/pi-lib
--- scripts/pi-server.sh
+++ scripts/pi-server.sh
@@ -2,6 +2,9 @@ mkdir -p .pi-lib/tools
 mkdir -p .pi-lib/server
 
 # ts_node can't run TypeScript inside node_modules
+# see https://github.com/TypeStrong/ts-node/issues/158
+# that led to this convoluted workaround
+
 if [ -d "node_modules/pi-lib/tools" ]; then
   cp -r node_modules/pi-lib/tools .pi-lib/
   ts-node .pi-lib/tools/homify.ts --server
@@ -9,7 +12,8 @@ else
   ts-node tools/homify.ts --server
 fi
 
-# ts_node can't run TypeScript inside node_modules
+# awkward because of my poor bash confidence!
+
 if [ -d "node_modules/pi-lib/server" ]; then
   if [ ! -L "node_modules/pi-lib/server" ]; then
     mv node_modules/pi-lib/server/* .pi-lib/server/
